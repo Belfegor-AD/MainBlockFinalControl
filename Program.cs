64aa8a654cf2c44db9a5152c1f4567207b3f4dae
Andrey Plotnikov <>
2024-01-10 10:43:33 +0700

    Draft version.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray()
+{
+    Console.WriteLine("Введите элементы массива через запятую: ");
+    string input = Console.ReadLine()!;
+    string[] array = input.Split(',');
+    return array;
+}
+
+string[] FilterArray(string[] array)
+{
+    int count = 0;
+    foreach(string i in array){
+        if (i.Length <= 3){
+            count++;
+        }
+    }
+
+    int j = 0;
+    string[] newArray = new string[count];
+    foreach(string i in array){
+        if (i.Length <= 3){
+            newArray[j] = i;
+            j++;
+        }
+    }
+    return newArray;
+}
+
+// string[] inputArray = CreateArray();
+// string[] filteredArray = FilterArray(inputArray);
+
+Console.WriteLine("Результат:");
+foreach (string i in FilterArray(CreateArray()))
+{
+Console.Write(i + ", ");
+}
